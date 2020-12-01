e5ec473bf213ecf6e887e443e30be74e1201e9fc
Jens Schreiber <jens.schreiber@uni-kassel.de>
2020-12-01 14:14:33 +0100

    created error matrix

--- csge/csge.py
+++ csge/csge.py
@@ -35,53 +35,29 @@ class CoopetitiveSoftGatingEnsemble(BaseEstimator):
         self.n_jobs = n_jobs
         self.n_cv_out_of_sample_error = n_cv_out_of_sample_error
         self.ensemble_members = None
+        self.error_matrix = None
 
-    def _get_global_error(self, X, y):
-        if self.ensemble_members is None:
-            raise NotFittedError("Ensemble members are not fitted.")
+    def _get_global_error(self):
+        self.global_errors = np.ones_like(self.error_matrix) * self.error_matrix.mean(
+            1
+        ).reshape(-1, 1)
 
+    def _create_error_matrix(self, X, y):
+        self.error_matrix = None
         global_errors = []
         num_samples = len(X)
+        # TODO what in case of dict for len(self.ensembles)
+        self.error_matrix = np.ones((len(self.ensemble_members), num_samples))
 
-        for ensemble_member in self.ensemble_members:
-            cur_ensemble_member_error = 0
+        for ens_id, ensemble_member in enumerate(self.ensemble_members):
             for idx, (_, test_index) in enumerate(self.train_test_indexes):
-                pred = ensemble_member[idx].predict(X[test_index])
-                cur_ensemble_member_error += self.error_function(y[test_index], pred)
-
-            cur_ensemble_member_error /= self.n_cv_out_of_sample_error
-            global_errors.append(cur_ensemble_member_error)
-
-        self.global_errors = np.hstack(
-            [
-                np.ones(num_samples).reshape((-1, 1)) * np.mean(val)
-                for val in global_errors
-            ]
-        )
-
-    def _get_local_error(self, X, y):
-        if self.ensemble_members is None:
-            raise NotFittedError("Ensemble members are not fitted.")
-
-        local_errors = []
-        num_samples = len(X)
-
-        for ensemble_member in self.ensemble_members:
-            cur_ensemble_member_error = np.ones((num_samples, 1))
-            for i, (_x, _y) in enumerate(zip(X, y)):
-                for idx, (_, test_index) in enumerate(self.train_test_indexes):
-                    preds = ensemble_member[idx].predict(X[test_index]).reshape(-1, 1)
-
-                    for j in range(len(test_index)):
-                        cur_ensemble_member_error[i, 0] += self.error_function(
-                            np.array(y[test_index][j]), np.array(preds[j])
-                        )
-
-            local_errors.append(
-                cur_ensemble_member_error / self.n_cv_out_of_sample_error
-            )
-
-        self.local_errors = np.concatenate(local_errors, axis=1)
+                preds = ensemble_member[idx].predict(X[test_index]).reshape(-1, 1)
+                for sample_id in range(len(test_index)):
+                    self.error_matrix[
+                        ens_id, sample_id * idx + sample_id
+                    ] = self.error_function(
+                        np.array(y[test_index][sample_id]), np.array(preds[sample_id])
+                    )
 
     def fit(self, X, y):
         # TODO: add time dependent weighting
@@ -104,8 +80,8 @@ class CoopetitiveSoftGatingEnsemble(BaseEstimator):
             self.ensemble_members.append(cv_ensembles)
 
         # TODO: create error matrix
-        self._get_global_error(X, y)
-        self._get_local_error(X, y)
+        self._create_error_matrix(X, y)
+        self._get_global_error()
 
     def predict(self, X):
         return self.ensemble_members
