039a30d3683dc5fe9a60a0846682f8e867b19703
Stephan Kutzner <kutzner.stephan@gmail.com>
2021-01-27 15:27:12 +0100

    #3 Add docstrings to public methods, change helpers for fit to be protected

--- csge/csge.py
+++ csge/csge.py
@@ -28,6 +28,25 @@ class CoopetitiveSoftGatingEnsemble(BaseEstimator):
         eta=[3.5, 3.5, 3.5],
         n_jobs=1,
     ):
+        """
+
+        Parameters
+        ----------
+        ensembles : list of classes
+            all classes must have .fit() and .predict()
+        error_function : function
+            the metric/function used to calculate the error for a given input
+        optimization_method : function
+            currently not used
+        n_cv_out_of_sample_error : int
+            defines the amount of chunks the train data is to be split
+        model_forecast_local_error : function
+            method used to calculate the local error
+        eta : int
+            defines the linearity of the weighting. Greater values result in a more selective sensemble
+        n_jobs : int
+            currently not used
+        """
         # TODO: ensembles as list or dict,in case of list default parameters, otherwise those provided by the dict
         # TODO: add check if parameters for ensemble member are correct
         # TODO: add function to get name from ensemble member
@@ -44,6 +63,18 @@ class CoopetitiveSoftGatingEnsemble(BaseEstimator):
         self.model_forecast_local_error = model_forecast_local_error
 
     def sum_all_weights(self, weights):
+        """
+        Calculate the sum of a given list of weights
+        Parameters
+        ----------
+        weights : list of numpy.array
+            the weights of all ensemble members that are to be summed
+        Returns
+        -------
+        numpy.array
+            sum of the given weights
+        """
+        #ToDo: leadtime_k, number_of_targets not defined
         sum_of_all_weights = np.full((self.leadtime_k, self.number_of_targets), 0.0)
 
         for weight in self.weights:
@@ -51,7 +82,18 @@ class CoopetitiveSoftGatingEnsemble(BaseEstimator):
 
         return sum_of_all_weights
 
-    def set_ensemble_parameters(self, parameters = []):
+    def set_ensemble_parameters(self, parameters=[]):
+        """
+        Use the given list to set the ensemble member's parameters
+        Parameters
+        ----------
+        parameters : list of dictionaries
+            each dictionary contains pairs of 'parameter: value'
+            order is equal to self.ensembles_types
+        Returns
+        -------
+
+        """
         try:
             assert len(parameters) == len(self.ensembles_types)
             for params in parameters:
@@ -100,6 +142,25 @@ class CoopetitiveSoftGatingEnsemble(BaseEstimator):
         self.error_matrix = self.error_matrix.transpose()
 
     def fit(self, X, y):
+        """
+        Fit the ensemble members to the given input data
+
+        First: create multiple objects of each ensemble member, and fit each to a subset of the input.
+        Second: use the ensemble members to calculate the global error
+        Third: create one object for each ensemble type, and fit it to the whole dataset
+        Fourth: create one predictor for each ensemble type, and fit it to predict the local error for a given input
+
+        Parameters
+        ----------
+        X : numpy.array
+            shape: [samples, features]
+        y : numpy.array
+            shape: [samples, 1]
+
+        Returns
+        -------
+
+        """
         # TODO: add time dependent weighting
         self.ensemble_members = []
         num_samples = len(X)
@@ -110,22 +171,22 @@ class CoopetitiveSoftGatingEnsemble(BaseEstimator):
         ]
 
         # fit ensemble members to create out of sample errors
-        self.fit_out_of_sample_ensembles(X, y)
+        self._fit_out_of_sample_ensembles(X, y)
 
         # same as the local error
         self._create_error_matrix(X, y)
         self._get_global_error()
 
         # refit ensemble members on complete data
-        self.fit_ensembles_for_prediction(X, y)
+        self._fit_ensembles_for_prediction(X, y)
 
         # fit to pred local error
-        self.fit_local_error_forecast(X)
+        self._fit_local_error_forecast(X)
 
         # TODO: implement time dependent weighting
         # TODO: implement soft gating
 
-    def fit_local_error_forecast(self, X):
+    def _fit_local_error_forecast(self, X):
         self.local_error_forecaster = []
 
         for id_ens in range(len(self.ensemble_members)):
@@ -133,7 +194,7 @@ class CoopetitiveSoftGatingEnsemble(BaseEstimator):
                 self.model_forecast_local_error().fit(X, self.error_matrix[:, id_ens])
             )
 
-    def fit_out_of_sample_ensembles(self, X, y):
+    def _fit_out_of_sample_ensembles(self, X, y):
         for i, ensemble in enumerate(self.ensembles_types):
             kf = KFold(n_splits=self.n_cv_out_of_sample_error)
             cv_ensembles = []
@@ -145,7 +206,7 @@ class CoopetitiveSoftGatingEnsemble(BaseEstimator):
                 cv_ensembles.append(model)
             self.ensemble_members.append(cv_ensembles)
 
-    def fit_ensembles_for_prediction(self, X, y):
+    def _fit_ensembles_for_prediction(self, X, y):
         self.ensemble_members = []
         for i, ensemble in enumerate(self.ensembles_types):
             model = ensemble()
@@ -168,18 +229,42 @@ class CoopetitiveSoftGatingEnsemble(BaseEstimator):
         return local_errors
 
     def _weight_forecasts(self, X, predictions):
+        # Normalize the global error of all ensemble members.
+        # Apply the softgating function to select the linearity.
+        # Shape: [1, len(ensemble_members)]
         normalized_global_error = self._normalize_weighting(self.global_errors)
         normalized_global_error = utils.soft_gating_formular(normalized_global_error, self.eta[0])
 
+        # Predict the local error of all ensemble members for each input separately.
+        # Apply the softgating function to select the linearity.
+        # Shape: [len(X), len(ensemble_members)]
         self.local_errors = 1 / self._pred_local_error(X)
         self.local_errors = utils.soft_gating_formular(self.local_errors, self.eta[1])
 
+
+        # Multiply both error types.
+        # This weights the local errors, i.e. the error of a given input, with the average error of the ensemble member
+        # over the whole input space it has been trained on.
+        # This results in selecting the best best ensemble member for a given input (local space), with respect to
+        # its overall performance.
         final_weighting = self.local_errors * normalized_global_error
         self.final_weighting = self._normalize_weighting(final_weighting)
 
         return (predictions * self.final_weighting).sum(1)
 
     def predict(self, X):
+        """
+        Predict with all enselbme members, and combine their results according to multiple error types.
+        Parameters
+        ----------
+        X : numpy.array
+            shape: [samples, features]
+
+        Returns
+        -------
+        numpy.array
+            shape: [samples, 1]
+        """
         predictions_ensembles = self._pred_all_ensembles(X)
         predictions = self._weight_forecasts(X, predictions_ensembles)
 
